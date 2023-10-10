8cd94ad61f720dac9585ea4094fc07b5399d9f21
Gijs Burghoorn <me@gburghoorn.com>
2023-10-11 01:19:11 +0200

    Add DSL for defining intrinsics

--- generate.py
+++ generate.py
@@ -23,19 +23,17 @@ def lmul_to_str(lmul: int) -> str:
         1: "f8",
         2: "f4",
         3: "f2",
-        4: "1",
-        8: "2",
+        4:  "1",
+        8:  "2",
         16: "4",
         32: "8",
     }[lmul]
-
 
 def init_vreg(eew: int, lmul: int) -> RustType:
     assert eew in EEWS
     assert lmul in LMULS
 
-    LMUL_STR = lmul_to_str(lmul)
-    TY = f"vint{eew}m{LMUL_STR}"
+    TY = f"vint{eew}m{lmul_to_str(lmul)}"
     return RustType(RustTypeVariant.VREG, TY, TY)
 
 def init_const_ptr(bits: int) -> RustType:
@@ -55,67 +53,24 @@ class RustParams:
     def __init__(self, params: List[Tuple[str, RustType]]) -> None:
         self.params = params
 
-    def has_numbered_xregs(self) -> bool:
-        return [param[1].variant == RustTypeVariant.XREG for param in self.params].count(True) > 1
-
-    def has_numbered_vregs(self) -> bool:
-        return [param[1].variant == RustTypeVariant.VREG for param in self.params].count(True) > 1
-
-    def get_var_ident(self, idx: int) -> str:
-        xreg_idx = 0
-        vreg_idx = 0
-
-        has_numbered_xregs = self.has_numbered_xregs()
-        has_numbered_vregs = self.has_numbered_vregs()
-
-        for i, (ident, ty) in enumerate(self.params):
-            assert not(ty.variant != RustTypeVariant.XREG and ident == "vl")
-
-            if i == idx:
-                if ident == "vl":
-                    return "vl"
-
-                num = ""
-                if ty.variant == RustTypeVariant.VREG and has_numbered_vregs:
-                    num = f"{vreg_idx}"
-                if ty.variant == RustTypeVariant.XREG and has_numbered_xregs:
-                    num = f"{xreg_idx}"
-
-                return f"{ident}{num}" 
-
-            if ident == "vl":
-                continue
-
-            if ty.variant == RustTypeVariant.VREG and has_numbered_vregs:
-                vreg_idx += 1
-            if ty.variant == RustTypeVariant.XREG and has_numbered_xregs:
-                xreg_idx += 1
-
-        raise Exception("Variable index out of range")
-
     def as_rust_with_types(self):
-        s = []
-
-        for i, (_, ty) in enumerate(self.params):
-            s.append(f"{self.get_var_ident(i)}: {ty.RUST_TYPE}")
-
-        return ', '.join(s)
+        return ', '.join(
+            [f"{ident}: {ty.RUST_TYPE}" for (ident, ty) in self.params]
+        )
 
     def as_llvm_with_types(self):
-        s = []
-
-        for i, (_, ty) in enumerate(self.params):
-            s.append(f"{self.get_var_ident(i)}: {ty.LLVM_TYPE}")
-
-        return ', '.join(s)
+        return ', '.join(
+            [f"{ident}: {ty.LLVM_TYPE}" for (ident, ty) in self.params]
+        )
 
     def as_llvm_vars(self):
-        s = []
-
-        for i, (_, ty) in enumerate(self.params):
-            s.append(f"{self.get_var_ident(i)} as {ty.LLVM_TYPE}")
-
-        return ', '.join(s)
+        return ', '.join(
+            [
+                f"{ident} as {ty.LLVM_TYPE}" if ty.LLVM_TYPE != ty.RUST_TYPE
+                else ident
+                for (ident, ty) in self.params
+            ]
+        )
 
     def is_empty(self) -> bool:
         return len(self.params) == 0
@@ -155,47 +110,129 @@ pub unsafe fn {name}({params}) {ret_type}{{
         llvm_ret_type = "" if ret_type == None else f" -> {ret_type.LLVM_TYPE}",
         poison_param = "" if not(poison) else f"poison!(), ",
         param_vars = params.as_llvm_vars(),
-        ret_type_as = "" if ret_type == None else f" as {ret_type.RUST_TYPE}",
+        ret_type_as = "" if (
+            ret_type == None or 
+            ret_type.LLVM_TYPE == ret_type.RUST_TYPE
+        ) else f" as {ret_type.RUST_TYPE}",
     )
 
-def struct_str(
-    elem: int,
-    lmul: int,
-) -> str:
-    STRUCT="""
-#[repr(simd, scalable({elem})]
-#[allow(non_camel_case_types)]
-pub struct {name} {{
-    _ty: [u{elem}],
-}}
-    """.strip()
+def parse_type(s: str, elem: int, lmul) -> RustType:
+    s = s.lower().strip()
+    
+    if s == "xr":
+        return XREG
+    elif s == "vr":
+        return VREG(elem, lmul)
+    elif s == "&e":
+        return CONST_PTR(elem)
+    elif s == "*e":
+        return MUT_PTR(elem)
+    
+    raise Exception(f"Unknown explict type for '{s}'")
 
-    return STRUCT.format(
-        elem = elem,
-        name = VREG(elem, lmul).RUST_TYPE,
-    )
+def parse_param(s: str, elem: int, lmul: int) -> Tuple[Tuple[str, RustType], bool, str]:
+    param_end = s.find(',')
+    is_end = param_end < 0
+    if param_end < 0:
+        param_end = s.find(')')
+    if param_end < 0:
+        raise Exception("Param has no valid end")
+    
+    param_str = s[:param_end].strip()
+    
+    explicit_ty_separator = param_str.find(':')
+    
+    if explicit_ty_separator < 0:
+        ident = param_str
+        
+        if ident.startswith("vs"):
+            ty = VREG(elem, lmul)
+        elif ident.startswith("rs"):
+            ty = XREG
+        elif ident == "vl":
+            ty = XREG
+        elif ident == "_p":
+            ty = VREG(elem, lmul)
+        else:
+            raise Exception(f"Unknown implicit type for {ident}")
+    else:
+        ident = param_str[:explicit_ty_separator]
+        ty = parse_type(param_str[explicit_ty_separator+1:], elem, lmul) 
+    
+    return ((ident, ty), is_end, s[param_end+1:])
 
-def generate_unit_strided_memory():
-    for elem in EEWS:
-        for lmul in LMULS:
-            print(intrinsic_fn_str(
-                name = f"vle{elem}_m{lmul}_v",
-                params = RustParams([("base", CONST_PTR(elem)), ("vl", XREG)]),
-                llvm_link = f"vle.nxv{lmul}i{elem}.i64",
-                poison = VREG(elem, lmul),
-                ret_type = VREG(elem, lmul),
-            ))
+def parse_intrinsic_str(s: str, elem: int, lmul: int):
+    name_end = s.find('(')
+    if name_end < 0:
+        raise Exception("NO PARAMS")
+    name = s[:name_end].strip()
+    if len(name) == 0:
+        raise Exception("NO NAME")
+    name = name.format(E = elem, L = lmul)
+    
+    s = s[name_end+1:].strip()
+    params = []
+    
+    ((ident, ty), _, s) = parse_param(s, elem, lmul)
+    s = s.strip()
+    
+    if ident == "_p":
+        poison = ty
+    else:
+        poison = None
+        params.append((ident, ty))
+    
+    while True:
+        (param, is_end, s) = parse_param(s, elem, lmul)
+        s = s.strip()
+        
+        params.append(param)
+        
+        if is_end:
+            break
+            
+    sig_end = s.find(':')
+    if sig_end < 0:
+        raise Exception("No LLVM LINK")
+    
+    if s.startswith("->"):
+        ret_type = parse_type(s[2:sig_end].strip(), elem, lmul)
+    else:
+        ret_type = None
+        
+    s = s[sig_end+1:]
+    
+    llvm_link = s.strip()
+    llvm_link = llvm_link.replace("NXV", f"nxv{lmul}i{elem}")
+    llvm_link = llvm_link.replace("iX", f"i64")
+    
+    assert name.lower() == name
+    assert llvm_link.lower() == llvm_link
+    
+    return {
+        "name": name,
+        "params": RustParams(params),
+        "llvm_link": llvm_link,
+        "poison": poison,
+        "ret_type": ret_type,
+    }  
+    
+def G(intrinsic_strs: List[str]):
+    for s in intrinsic_strs:
+        for elem in EEWS:
+            for lmul in LMULS:
+                intrinsic = parse_intrinsic_str(s, elem, lmul)
+                print(intrinsic_fn_str(
+                    name = intrinsic["name"],
+                    params = intrinsic["params"],
+                    llvm_link = intrinsic["llvm_link"],
+                    poison = intrinsic["poison"],
+                    ret_type = intrinsic["ret_type"],
+                ))
 
-            print(intrinsic_fn_str(
-                name = f"vse{elem}_m{lmul}_v",
-                params = RustParams([("rd", VREG(elem, lmul)), ("base", CONST_PTR(elem)), ("vl", XREG)]),
-                llvm_link = f"vse.nxv{lmul}i{elem}.i64",
-                poison = None,
-                ret_type = None,
-            ))
-
-            print("")
-
+print("""
+/// AUTOGENERATED FILE
+""".strip())
 
 print("""
 macro_rules! undef {
@@ -207,6 +244,20 @@ macro_rules! undef {
 
 for elem in EEWS:
     for lmul in LMULS:
-        print(struct_str(elem, lmul))
-
-generate_unit_strided_memory()
+        print("""
+#[repr(simd, scalable({elem})]
+#[allow(non_camel_case_types)]
+pub struct {name} {{
+    _ty: [u{elem}],
+}}
+        """.strip().format(elem = elem, name = VREG(elem, lmul).RUST_TYPE))
+        
+G([
+    "vle{E}_m{L}_v(_p,base:&E,vl)->VR:vle.NXV.iX",
+    "vse{E}_m{L}_v(vs,base:*E,vl):vse.NXV.iX",
+    "vlse{E}_m{L}_v(_p,base:&E,bstride:XR,vl)->VR:vlse.NXV.iX",
+    "vsse{E}_m{L}_v(vs,base:*E,bstride:XR,vl):vsse.NXV.iX",
+    "vle{E}ff_m{L}_v(_p,base:&E,vl)->VR:vleff.NXV.iX",
+    "vadd_e{E}_m{L}_vv(_p,vs2,vs1,vl)->VR:vadd.NXV.NXV.iX",
+    "vsub_e{E}_m{L}_vv(_p,vs2,vs1,vl)->VR:vsub.NXV.NXV.iX",
+])
